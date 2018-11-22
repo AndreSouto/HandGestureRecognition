080117dd4243a68542d690bbc87c93cfd069aef1
AndreSouto <andre_luisferreira@hotmail.com>
2018-11-21 22:33:44 -0200

    .m files

---
+++ main.m
@@ -0,0 +1,13 @@
+% Final Project - IPI - Hand Gesture Recognition  %
+% Source: Hand Gesture Recognition based on Shape %
+% Parameters                                      %
+
+% Rafaela Sinhoroto - %
+% André Luis Souto - 140016261 %
+% Raphael %
+
+function main()
+
+
+
+end
